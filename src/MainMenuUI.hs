9ed2bfc3d457df0605fc24818b1279ac40c42170
Piotr Rudnicki <deamondev@gmail.com>
2022-06-30 12:15:35 +0200

    menu ops

--- src/MainMenuUI.hs
+++ src/MainMenuUI.hs
@@ -2,13 +2,18 @@
 module MainMenuUI where
 import Brick
 import AppState
-import qualified Graphics.Vty as V
+import qualified Graphics.Vty as V 
+import qualified Brick.Widgets.Center as C
+import View (mainMenuOps)
 
 
 
 draw :: AppState -> [Widget ()]
-draw appState = [str "MainMenuUI"]
+draw appState = [ui]
+            where 
+              ui = C.vCenter $ vBox $ map str mainMenuOps
 
 handleEvent :: AppState -> BrickEvent () () -> EventM () (Next AppState)
 handleEvent appState e@(VtyEvent (V.EvKey (V.KChar 'j') [])) = continue $ minusOne appState
 handleEvent appState e@(VtyEvent (V.EvKey (V.KChar 'k') [])) = continue $ addOne appState
+handleEvent appState e@(VtyEvent (V.EvKey V.KEsc []))        = halt appState
