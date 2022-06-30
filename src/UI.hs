773488ae4f87e2818453bd88a69cb90f789e6ee6
Piotr Rudnicki <deamondev@gmail.com>
2022-06-30 19:45:37 +0200

    goodbye message:

--- src/UI.hs
+++ src/UI.hs
@@ -4,8 +4,8 @@ import AppState (AppState, initialAppState)
 import Brick
 import Control.Monad (void)
 import qualified Graphics.Vty as V
-import qualified MainMenuUI
 import qualified HelpUI
+import qualified MainMenuUI
 import View
 
 app :: App AppState () ()
@@ -19,19 +19,19 @@ app =
     }
 
 main :: IO ()
-main = void $ defaultMain app initialAppState
+main = void $ defaultMain app initialAppState >> putStrLn "Thank you for using Todo app!"
 
 drawUI :: AppState -> [Widget ()]
 drawUI appState = case appState of
   MainMenuView {} -> MainMenuUI.draw appState
   TodoListView {} -> undefined
-  HelpView        -> HelpUI.draw appState
+  HelpView -> HelpUI.draw appState
 
 handleEvent :: AppState -> BrickEvent () () -> EventM () (Next AppState)
 handleEvent appState = case appState of
   MainMenuView {} -> MainMenuUI.handleEvent appState
   TodoListView {} -> undefined
-  HelpView        -> HelpUI.handleEvent appState
+  HelpView -> HelpUI.handleEvent appState
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr []
