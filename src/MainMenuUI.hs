260e574905957e90fc2b19d582176656807b559d
Piotr Rudnicki <deamondev@gmail.com>
2022-06-30 12:07:39 +0200

    added main menu view

--- src/MainMenuUI.hs
+++ src/MainMenuUI.hs
@@ -1,10 +1,14 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module MainMenuUI where
 import Brick
 import AppState
+import qualified Graphics.Vty as V
+
 
 
 draw :: AppState -> [Widget ()]
 draw appState = [str "MainMenuUI"]
 
 handleEvent :: AppState -> BrickEvent () () -> EventM () (Next AppState)
-handleEvent appState = undefined
+handleEvent appState e@(VtyEvent (V.EvKey (V.KChar 'j') [])) = continue $ minusOne appState
+handleEvent appState e@(VtyEvent (V.EvKey (V.KChar 'k') [])) = continue $ addOne appState
