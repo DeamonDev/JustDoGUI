63bcf5adda08074e05c949b9bfcfa8e2d5de4e3e
Piotr Rudnicki <deamondev@gmail.com>
2022-07-12 22:09:11 +0200

    getting rid on old render code

--- src/utils/ListRender.hs
+++ src/utils/ListRender.hs
@@ -32,29 +32,3 @@ applyStyles (x:xs) w = applyStyles xs $ withAttr x w
 
 renderWithRightPadding' :: [(String, Maybe [A.AttrName])] -> [Widget ()]
 renderWithRightPadding' l = map (padRight Max) $ render' l 
-
-
-render :: Int -> A.AttrName -> [String] -> [Widget ()]
-render index selectedAttr l = listRenderAux 0 index l []
-  where
-    listRenderAux curr index l acc
-      | curr >= length l = acc
-      | curr == index =
-        let newAcc = acc ++ [withAttr selectedAttr $ str (l !! curr)]
-         in listRenderAux (curr + 1) index l newAcc
-      | curr /= index =
-        let newAcc = acc ++ [str (l !! curr)]
-         in listRenderAux (curr + 1) index l newAcc
-
-
-renderWithPadding :: Int -> A.AttrName -> [String] -> [Widget Name]
-renderWithPadding index selectedAttr l = listRenderAux 0 index l []
-  where
-    listRenderAux curr index l acc
-      | curr >= length l = acc
-      | curr == index =
-        let newAcc = acc ++ [padRight Max $ withAttr selectedAttr $ str (l !! curr)]
-         in listRenderAux (curr + 1) index l newAcc
-      | curr /= index =
-        let newAcc = acc ++ [padRight Max $ str (l !! curr)]
-         in listRenderAux (curr + 1) index l newAcc
